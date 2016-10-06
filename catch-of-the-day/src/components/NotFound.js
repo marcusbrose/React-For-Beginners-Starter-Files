692b2f2cf36028aa83f2054238ea151ed89b452e
Marcus Brose <contact@marcusbrose.com>
2016-10-06 18:22:57 +0200

    routing

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// stateless component, has only render method
+const NotFound = (props) => {
+  return (
+    <h2>
+      Not Found
+    </h2>
+  )
+}
+
+export default NotFound
