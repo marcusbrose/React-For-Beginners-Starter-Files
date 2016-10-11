c61caec39e5a793ae0b408b4f16e6cec8fd2ea0f
Marcus Brose <contact@marcusbrose.com>
2016-10-11 23:14:07 +0200

    authentication

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import AddFishForm from './AddFishForm'
+import base from '../base'
 
 const propTypes = {
   fishes: React.PropTypes.object.isRequired,
@@ -7,6 +8,7 @@ const propTypes = {
   removeFish: React.PropTypes.func.isRequired,
   addFish: React.PropTypes.func.isRequired,
   loadSamples: React.PropTypes.func.isRequired,
+  storeId: React.PropTypes.string.isRequired,
 }
 
 class Inventory extends React.Component {
@@ -15,7 +17,24 @@ class Inventory extends React.Component {
     super()
 
     this.handleChange = this.handleChange.bind(this)
+    this.authenticate = this.authenticate.bind(this)
+    this.authHandler = this.authHandler.bind(this)
+    this.logout = this.logout.bind(this)
+    this.renderLogin = this.renderLogin.bind(this)
     this.renderInventory = this.renderInventory.bind(this)
+
+    this.state = {
+      uid: null,
+      owner: null
+    }
+  }
+
+  componentDidMount() {
+    base.onAuth((user) => {
+      if (user) {
+        this.authHandler(null, { user })
+      }
+    })
   }
 
   handleChange(event, key) {
@@ -25,6 +44,54 @@ class Inventory extends React.Component {
       [event.target.name]: event.target.value
     }
     this.props.updateFish(key, updatedFish)
+  }
+
+  authenticate(provider) {
+    base.authWithOAuthPopup(provider, this.authHandler)
+  }
+
+  authHandler(error, authData) {
+    if (error) {
+      console.log(error)
+      return
+    }
+
+    // grab the store info
+    const storeRef = base.database().ref(this.props.storeId)
+
+    // query the firebase once for the store data
+    storeRef.once('value', (snapshot) => {
+      const data = snapshot.val() || {}
+      console.log(data);
+      // claim it if there is no owner already
+      if ( ! data.owner) {
+        storeRef.set({
+          owner: authData.user.uid
+        })
+      }
+
+      this.setState({
+        uid: authData.user.uid,
+        owner: data.owner || authData.user.uid
+      })
+    })
+  }
+
+  logout() {
+    base.unauth()
+    this.setState({
+      uid: null
+    })
+  }
+
+  renderLogin() {
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="facebook" onClick={() => this.authenticate('facebook')}>Login with Facebook</button>
+      </nav>
+    )
   }
 
   renderInventory(key) {
@@ -37,7 +104,7 @@ class Inventory extends React.Component {
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold out!</option>
         </select>
-        <textarea type="text" name="description" value={fish.description} placeholder="Fish Description" onChange={(e) => this.handleChange(e, key)}></textarea>
+        <textarea type="text" name="desc" value={fish.desc} placeholder="Fish Description" onChange={(e) => this.handleChange(e, key)}></textarea>
         <input type="text" name="image" value={fish.image} placeholder="Fish Image" onChange={(e) => this.handleChange(e, key)} />
         <button type="submit" onClick={() => this.props.removeFish(key)}>Remove Fish</button>
       </div>
@@ -45,9 +112,24 @@ class Inventory extends React.Component {
   }
 
   render() {
+    const logoutButton = <button onClick={() => this.logout()}>Log Out!</button>
+    // check if logged in
+    if ( ! this.state.uid) {
+      return <div>{this.renderLogin()}</div>
+    }
+    // check if user is owner of the store
+    if (this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>Sorry, you aren't the owner of this store!</p>
+          {logoutButton}
+        </div>
+      )
+    }
     return (
       <div>
         <h2>Inventory</h2>
+        {logoutButton}
         {Object.keys(this.props.fishes).map(this.renderInventory)}
         <AddFishForm addFish={this.props.addFish} />
         <button onClick={this.props.loadSamples}>
